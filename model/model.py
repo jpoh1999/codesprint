32f028c7dcc031ebe781fb7a336ef788c5ce34bb
pohja1999 <98691869+pohja1999@users.noreply.github.com>
2024-10-11 19:46:35 +0800

    Added model folder

---
+++ model/model.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+# import torch
+
+
+class Model() :
+    """
+        Smart Shuffling Model based on Deep Reinforcement Learning Approach 
+    """
+
+    def __init__(self) :
+        """
+            Define all configuration variables
+        """
+        pass
+
+    def train(self) :
+        pass        
+    
+    def test(self) :
+        """
+            Solve the model
+        """
+        pass
